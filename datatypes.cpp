f0c3941a9fe161bc5c8dbf765b8104ba026d0660
Saurav Paul <72635245+paulsaurav@users.noreply.github.com>
2024-01-27 14:01:58 +0530

    datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // integer datatype
+    int integerValue = 20;
+    cout << "Stored integer value is: " << integerValue << endl;
+
+    // Floating point datatype
+    float floatValue = 3.14;
+    cout << "Stored float value is: " << floatValue << endl;
+
+    // Character datatype
+    char charValue = 'S';
+    cout << "Stored character value is: " << charValue << endl;
+
+    // Boolean datatype
+    bool boolValue = true;
+    cout << "Stored boolean value is: " << boolValue << endl;
+
+    return 0;
+}
